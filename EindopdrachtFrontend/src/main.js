2824433f46aacf8cc5426435aacb4c850d85f405
LukoGi <lukopecco@gmail.com>
2024-04-06 23:24:23 +0200

    Basic router and header and home setup (frontend)

--- EindopdrachtFrontend/src/main.js
+++ EindopdrachtFrontend/src/main.js
@@ -1,6 +1,16 @@
-import './assets/main.css'
+import './assets/main.scss'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
---
+++ EindopdrachtFrontend/src/views/home/homeview.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="home">
+      <h1>Welcome to Home View</h1>
+      <!-- Add more HTML here -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'HomeView',
+    data() {
+      return {
+        // Add your component's data here
+      };
+    },
+    methods: {
+      // Add your component's methods here
+    },
+    computed: {
+      // Add your component's computed properties here
+    },
+    mounted() {
+      // Code to run when the component is mounted
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .home {
+    /* Add your CSS styles for .home class here */
+  }
+  </style>
